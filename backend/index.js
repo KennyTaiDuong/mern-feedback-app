d0c256cf50e829d25d319e1416f7e79e88703e93
KennyTaiDuong <kennyduong537@gmail.com>
2023-10-03 20:06:00 -0500

    dockerized backend server

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method);
+  console.log(req.path, res, req.method);
   next();
 });
 
